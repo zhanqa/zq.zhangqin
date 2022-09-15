ac2b972916939f1539a2bfd98cbd4bf55444eac6
zhanqa <113295599+zhanqa@users.noreply.github.com>
2022-09-15 17:29:21 +0800

    Add files via upload
    学号：2020144145
    姓名：张勤
    第二次作业（1）

---
+++ exercise2.py
@@ -0,0 +1,8 @@
+from collections import defaultdict
+str=input("please input a string:")
+str=str.lower()
+chars=defaultdict(int)
+for char in str:
+    chars[char]+=1
+chars=sorted(chars.items(),key=lambda d:d[1],reverse=True)
+print(chars)
