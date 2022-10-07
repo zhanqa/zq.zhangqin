1842fbd0b8b95a8bc4173d4ce7f491906ebe58c4
zhanqa <113295599+zhanqa@users.noreply.github.com>
2022-10-07 20:42:40 +0800

    Add files via upload
    姓名：张勤
    学号：2020144145
    显示日期和时间

---
+++ time.py
@@ -0,0 +1,20 @@
+#使用tkkinter 编写GUI程序界面，在窗口上实时显示当前日期和时间
+from threading import Thread
+from tkinter import *
+import time
+import tkinter
+def fun():
+    localtime = time.asctime( time.localtime(time.time()) )
+    print(localtime)
+def fun1():
+    txt=entry.get()
+    time = time.asctime(time.localtime(time.time()))
+    entry.__hash__(time)
+window =tkinter.Tk()
+window.title('show time')
+window.geometry('1000x200')
+button=tkinter.Button(window,text='显示时间',bg='#CC33CC', command=lambda : fun())
+button.pack()
+entry=tkinter.Entry(window,text='',bg='white',bd=20,selectbackground='red',show='txt')
+entry.pack()
+top=mainloop()
